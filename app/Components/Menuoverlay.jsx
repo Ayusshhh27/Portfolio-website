3fc3521c3ed43f3729a404dee90ad01afaedbdcd
Ayush Chandra <ayushhchandra700@gmail.com>
2024-02-06 16:55:00 +0530

    fixed menuoverlay problem for deployment

--- app/Components/Menuoverlay.jsx
+++ app/Components/Menuoverlay.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import NavLink from "./NavLink";
+import Link from "next/link";
 
 const MenuOverlay = ({ links }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
         <li key={index}>
-          <NavLink href={link.path} title={link.title} />
+          <Link href={link.path} title={link.title} />
         </li>
       ))}
     </ul>
