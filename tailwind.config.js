a705b0b27715b34f13df48af25f50abff9853ddf
Ayush Chandra <ayushhchandra700@gmail.com>
2024-02-13 00:26:53 +0530

    Renamed some tailwind Css configs to fix unncessary warnings in terminal during launch.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    // "./app/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
